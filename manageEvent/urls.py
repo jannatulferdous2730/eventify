14287a90084448797f4971d9f1625be995b120a6
jannatulferdous2730 <jannatulferdous.2730@gmail.com>
2024-11-18 01:28:30 +0600

    further modifications

--- manageEvent/urls.py
+++ manageEvent/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('venue_page/', views.venue_page),
+    path('', views.index, name='index'),
+    path('venue_page/', views.venue_page, name='venue_page'),
 ] 
